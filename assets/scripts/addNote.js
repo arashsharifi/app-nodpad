9b0548d0a43bd93abadfb808d9f712eeedb85c42
arash amirsharifi <arashamirsharifi1970@gmail.com>
2023-07-26 15:31:55 +0330

    add newNote & toastify

--- assets/scripts/addNote.js
+++ assets/scripts/addNote.js
@@ -10,10 +10,55 @@ function eventListener() {
 
 function handelSubmit(e) {
     e.preventDefault()
-    const formData = Object.fromEntries(new FormData(form).entries())
-    if (formData.title && formData.text) {
-        console.log('yes it can submit')
+    const note = Object.fromEntries(new FormData(form).entries())
+    if (note.title && note.text) {
+        //add handeling
+        // console.log('yes it can submit')
+        note.id = Date.now()
+
+        addNewNote(note)
+
+        form.reset()
+
+        generatost("my note is add mylist",
+
+            "linear-gradient(to right,#6ab04c, #badc58)",
+
+            2000, () => location.assign("notes.html"))
     } else {
-        console.log('no it cant')
+        generatost('this is no text in hear',
+            "linear-gradient(to right, #eb4d4b, #ff7979)",
+
+            3000, () => location.assign("notes.html"))
     }
+}
+
+//add function for tost object
+function generatost(text, color, duration, callback) {
+    Toastify({
+        text: text,
+        duration: duration,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: color,
+        },
+        callback: callback
+
+    }).showToast();
+
+}
+
+function addNewNote(getvalueNote) {
+    const hasLocalStorage = localStorage.getItem('noteList')
+
+    const notes = hasLocalStorage ? JSON.parse(hasLocalStorage) : []
+
+    notes.push(getvalueNote)
+
+    console.log(notes)
+
+    localStorage.setItem('noteList', JSON.stringify(notes))
 }
