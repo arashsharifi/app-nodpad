842569e8b34d47d24a40c0690f61bf6a6e78bf0e
arash amirsharifi <arashamirsharifi1970@gmail.com>
2023-08-09 21:15:11 +0330

    fix scroll-y and add search input but not (debounce) lodash

--- assets/scripts/notes.js
+++ assets/scripts/notes.js
@@ -1,10 +1,10 @@
-const noNotes = document.querySelector('.no-notes');
-const cardContainer = document.querySelector('.cards');
-const tags = document.querySelectorAll('.tags span');
-console.log(tags)
+const noNotes = document.querySelector(".no-notes");
+const cardContainer = document.querySelector(".cards");
+const tags = document.querySelectorAll(".tags span");
+const search = document.querySelector("#search");
+console.log(tags);
 
-let tag = "#all"
-
+let tag = "#all";
 
 // console.log(noNotes);
 // console.log(cardContainer);
@@ -12,42 +12,42 @@ let tag = "#all"
 eventListener();
 
 function eventListener() {
+  document.addEventListener("DOMContentLoaded", bildCard);
 
-    document.addEventListener('DOMContentLoaded', bildCard)
+  cardContainer.addEventListener("click", removeClidElement);
 
-    cardContainer.addEventListener('click', removeClidElement)
+  tags.forEach((tag) => tag.addEventListener("click", changeTge));
 
-    tags.forEach(tag => tag.addEventListener('click', changeTge));
-
-
+  search.addEventListener("input", searchQuery);
 }
 
 function bildCard() {
-    const hasLocalStorage = localStorage.getItem('noteList')
-    if (hasLocalStorage == null || hasLocalStorage == "[]") {
-        noNotes.style.display = "block"
-    } else {
-        noNotes.style.display = "none"
-        const notes = hasLocalStorage ? JSON.parse(localStorage.getItem('noteList')) : []
+  const hasLocalStorage = localStorage.getItem("noteList");
+  if (hasLocalStorage == null || hasLocalStorage == "[]") {
+    noNotes.style.display = "block";
+  } else {
+    noNotes.style.display = "none";
+    const notes = hasLocalStorage
+      ? JSON.parse(localStorage.getItem("noteList"))
+      : [];
 
-        const notesOfTag = tag !== "#all" ? notes.filter((note) => note.category == tag) : notes
+    const notesOfTag =
+      tag !== "#all" ? notes.filter((note) => note.category == tag) : notes;
 
-
-        // console.log(notes)
-        if (notesOfTag.length > 0) {
-            generatNote(notesOfTag)
-        }
+    // console.log(notes)
+    if (notesOfTag.length > 0) {
+      generatNote(notesOfTag);
     }
+  }
 }
 
-
-
 function generatNote(arrayNotes) {
-    arrayNotes.forEach(note => {
-        // console.log(note)
-        const card = document.createElement('div')
-        card.classList = "card"
-        card.innerHTML = `
+  cardContainer.innerHTML = "";
+  arrayNotes.forEach((note) => {
+    // console.log(note)
+    const card = document.createElement("div");
+    card.classList = "card";
+    card.innerHTML = `
         <div class="card info-card" >
          <h6 class="cart-title">
           ${note.title}
@@ -62,43 +62,60 @@ function generatNote(arrayNotes) {
           <div>
         ${note.text}
         </div> 
-        `
-        // console.log(card)
-        cardContainer.appendChild(card)
-    });
+        `;
+    // console.log(card)
+    cardContainer.appendChild(card);
+  });
 }
 
 function deletNote(id) {
-    const getfromLS = JSON.parse(localStorage.getItem('noteList'));
-    console.log(getfromLS)
-    const updateNote = getfromLS.filter(notelist => notelist.id !== id)
-    localStorage.setItem('noteList', JSON.stringify(updateNote))
-
+  const getfromLS = JSON.parse(localStorage.getItem("noteList"));
+  console.log(getfromLS);
+  const updateNote = getfromLS.filter((notelist) => notelist.id !== id);
+  localStorage.setItem("noteList", JSON.stringify(updateNote));
 }
 
 function editNote(id) {
+  localStorage.setItem("editObject", id);
 
-    localStorage.setItem("editObject", id)
-
-    location.assign('add.html')
+  location.assign("add.html");
 }
 
 function removeClidElement(e) {
-    // console.log(e.target.classList)
-    if (e.target.classList.contains('bi')) {
-        e.target.parentElement.parentElement.parentElement.parentElement.remove()
-    } else {
-        console.log('no')
-    }
+  // console.log(e.target.classList)
+  if (e.target.classList.contains("bi")) {
+    e.target.parentElement.parentElement.parentElement.parentElement.remove();
+  } else {
+    console.log("no");
+  }
 }
 
 function changeTge() {
-    cardContainer.innerHTML = ''
-    tags.forEach(tag => tag.classList = '');
+  cardContainer.innerHTML = "";
+  tags.forEach((tag) => (tag.classList = ""));
 
-    this.classList = "active"
+  this.classList = "active";
 
-    tag = this.innerHTML
+  tag = this.innerHTML;
 
-    bildCard()
+  bildCard();
+}
+
+function searchQuery(e) {
+  console.log("input");
+  const noteList = JSON.parse(localStorage.getItem("noteList"));
+  //   console.log(noteList);
+
+  if (noteList) {
+    function mydebunc() {
+      const filterNoteBysearch = noteList.filter(
+        (note) =>
+          note.title.includes(e.target.value) ||
+          note.text.includes(e.target.value)
+      );
+      generatNote(filterNoteBysearch);
+    }
+    mydebunc();
+    _.debounce(mydebunc, 1000);
+  }
 }
